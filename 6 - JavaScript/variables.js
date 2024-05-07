389b35bf8e6eb7b91f79e80619134654acd5781f
Jared Neil A. Hortaleza <jaredneilhortaleza11@gmail.com>
2024-05-07 10:09:29 +0800

    finished CSS and added JavaScript

---
+++ 6 - JavaScript/variables.js
@@ -0,0 +1,23 @@
+console.log("Hello World!");
+
+// Variables = containers
+/* 
+    let - mutable/changeable value
+    const - unmutable/unchangeable value
+    var - same as let, just with a few scoping problems
+*/
+
+// let
+let firstName = "Jared";
+console.log("Firstname:", firstName);
+
+// const
+const lastName = "Hortaleza";
+console.log("Lastname:", lastName);
+
+// editing let
+firstName = "Ariston";
+console.log("Firstname:", firstName);
+
+// editing const?
+// lastName = "Fitzgerald";
